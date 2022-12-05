8761254c6e5d83fb8e720ac5c95a406373062c9f
Joe-Collins-1986 <collins.jw@btinternet.com>
2022-12-05 18:08:10 +0000

    set up POST for add catagory

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, redirect, request, url_for
 from taskmanager import app, db
 from taskmanager.models import Catagory, Task
 
@@ -6,3 +6,18 @@ from taskmanager.models import Catagory, Task
 @app.route("/")
 def home():
     return render_template("tasks.html")
+
+
+@app.route("/catagories")
+def catagories():
+    return render_template("catagories.html")
+
+
+@app.route("/add_catagory", methods=["GET", "POST"])
+def add_catagory():
+    if request.method == "POST":
+        catagory = Catagory(catagory_name=request.form.get("catagory_name"))
+        db.session.add(catagory)
+        db.session.commit()
+        return redirect(url_for("catagories"))
+    return render_template("add_catagory.html")
