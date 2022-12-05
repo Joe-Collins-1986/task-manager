3514b4185496d0645a8095bd440086180b038d3d
Joe-Collins-1986 <collins.jw@btinternet.com>
2022-12-05 17:05:12 +0000

    fix mobile menu link

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,3 +1,6 @@
-$(document).ready(function(){
-    $('.sidenav').sidenav();
-  });
+
+document.addEventListener("DOMContentLoaded", function() {
+  // sidenav initialization
+  let sidenav = document.querySelectorAll(".sidenav");
+  M.Sidenav.init(sidenav);
+});
