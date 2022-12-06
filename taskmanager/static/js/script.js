19a8c26bf3bce6d92c673d3d2fa158e53873839a
Joe-Collins-1986 <collins.jw@btinternet.com>
2022-12-06 17:34:32 +0000

    present added tasks to user

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,16 +1,21 @@
 
 document.addEventListener("DOMContentLoaded", function() {
-  // sidenav initialization
+  // sidenav initialisation
   let sidenav = document.querySelectorAll(".sidenav");
   M.Sidenav.init(sidenav);
 
-  // datepicker initialization
+  // datepicker initialisation
   let datepicker = document.querySelectorAll('.datepicker');
   M.Datepicker.init(datepicker, {
     format: "dd mmmm, yyyy",
     i18n: {done: "Select"}
   });
 
+  // select initialisation
   let selects = document.querySelectorAll('select');
   M.FormSelect.init(selects);
+
+  // collapsible initialisation
+  let collapsible = document.querySelectorAll('.collapsible');
+  M.Collapsible.init(collapsible);
 });
